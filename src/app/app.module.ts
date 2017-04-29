831115661658f4d21ad6e93478200f8668285dd2
Thamara Silva <thamara.msc@gmail.com>
2017-04-29 16:26:36 -0300

    authentication

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
+
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyCzPVnbOluYrrfwXnnifwTT6nwxDzKSeiA",
+    authDomain: "nearby-business-api.firebaseapp.com",
+    databaseURL: "https://nearby-business-api.firebaseio.com",
+    projectId: "nearby-business-api",
+    storageBucket: "nearby-business-api.appspot.com",
+    messagingSenderId: "640592206262"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
+  ],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
